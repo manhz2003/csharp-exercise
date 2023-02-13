6c1fd8dc455caebbe9f1c46afc7cb806d1851ec8
manhz2003 <manhthenguyen2003@gmail.com>
2023-02-13 19:40:12 +0700

    create new

---
+++ lab1/baiTap2/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("nhập vào chiều cao: ");
+        double h = double.Parse(Console.ReadLine());
+        Console.WriteLine("nhập vào bán kính: ");
+        double r = double.Parse(Console.ReadLine());
+         
+        double sxq = 2 * Math.PI * h * r;
+        Console.WriteLine("diện tích xung quanh của hình trụ là: {0:0.00}", sxq);
+
+        double vxq = Math.PI * r * r * h;
+        Console.WriteLine("thể tích hình trụ là: {0:0.00}", vxq);
+
+    }
+}
---
+++ lab1/baiTap3/ConsoleApp3/Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    enum So { Mot = 1, Hai, Ba, Bon, Nam, Sau, Bay, Tam, Chin, Muoi };
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhập vào một số: ");
+        int num = int.Parse(Console.ReadLine());
+
+        switch (num)
+        {
+            case 1:
+                Console.WriteLine("Một");
+                break;
+            case 2:
+                Console.WriteLine("Hai");
+                break;
+            case 3:
+                Console.WriteLine("Ba");
+                break;
+            case 4:
+                Console.WriteLine("Bốn");
+                break;
+            case 5:
+                Console.WriteLine("Năm");
+                break;
+            case 6:
+                Console.WriteLine("Sáu");
+                break;
+            case 7:
+                Console.WriteLine("Bảy");
+                break;
+            case 8:
+                Console.WriteLine("Tám");
+                break;
+            case 9:
+                Console.WriteLine("Chín");
+                break;
+            case 10:
+                Console.WriteLine("Mười");
+                break;
+            default:
+                Console.WriteLine("Không tìm thấy kết quả");
+                break;
+        }
+    }
+}
