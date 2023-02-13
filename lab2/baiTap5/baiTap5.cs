80b3b0cd3409173ecd252e0844199ad6ea0b49c9
anhz2003 <luonglananh2003@gmail.com>
2023-02-13 18:32:59 +0700

    go

---
+++ lab2/baiTap5/baiTap5.cs
@@ -0,0 +1,24 @@
+﻿namespace baiTap5
+{
+    internal class baiTap5
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập số n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int a = 0, b = 1, c;
+            Console.Write(a + " " + b);
+
+            for (int i = 2; i < n; i++)
+            {
+                c = a + b;
+                Console.Write(" " + c);
+                a = b;
+                b = c;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab3/lab3/so.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class so
+{
+    public int num;
+
+    public So(int n)
+    {
+        num = n;
+    }
+
+    public So Cong(So s2)
+    {
+        So s = new So(0);
+        s.num = num + s2.num;
+        return s;
+    }
+
+    public So Tru(So s2)
+    {
+        So s = new So(0);
+        s.num = num - s2.num;
+        return s;
+    }
+
+    public So Nhan(So s2)
+    {
+        So s = new So(0);
+        s.num = num * s2.num;
+        return s;
+    }
+
+    public So Chia(So s2)
+    {
+        So s = new So(0);
+        s.num = num / s2.num;
+        return s;
+    }
+}
